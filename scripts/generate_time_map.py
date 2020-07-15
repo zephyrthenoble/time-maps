e00e571d0329b2c55031a52defa7054870fd94c2
Mitchell Smith <zephyrthenoble@gmail.com>
2020-07-15 16:01:11 -0400

    removing OLD Twitter info

--- scripts/generate_time_map.py
+++ scripts/generate_time_map.py
@@ -1,23 +1,13 @@
 import click
 import time_maps
 
-SCATTER=0
-HEAT=1
-
-def my_secrets():  # store your twitter codes
-
-    d = {}
-    d["APP_KEY"] = "O17AbV1eqmbnbPFv0r9yjLzEL"
-    d["APP_SECRET"] = "SJiMvks1mwGqbX1PSkhQWRV5FQXqxhrcoBRaHy5H26eEdq9wS8"
-    d["my_access_token_key"] = "807227574-CPr89uEHp1EKI7mcaSwdLdhFXAZ1W0kVhRSP48ZA"
-    d["my_access_token_secret"] = "0PywOTj0wHVB84OjqPN5XS9NBUJy4yyNRrstzfsHjNcjO"
-
-    return d
+SCATTER = 0
+HEAT = 1
 
 
 @click.command()
-@click.option("--handle","-h", multiple=True)
-@click.option("--handle-file","-f", type=click.File())
+@click.option("--handle", "-h", multiple=True)
+@click.option("--handle-file", "-f", type=click.File())
 @click.option("--map-type", "-m", type=click.Choice(["heatmap", "scatter", "both"]), default="scatter")
 @click.option("--save-path", type=click.Path(exists=True), default=".")
 @click.option("--app-key", required=True)
@@ -25,8 +15,8 @@ def my_secrets():  # store your twitter codes
 #  @click.option("--access-key", required=True)
 #  @click.option("--access-secret", required=True)
 def generate_time_map(
-    handle, handle_file, map_type, save_path, app_key, app_secret
-    #  handle, handle_file, map_type, save_path, app_key, app_secret, access_key, access_secret
+        handle, handle_file, map_type, save_path, app_key, app_secret
+        #  handle, handle_file, map_type, save_path, app_key, app_secret, access_key, access_secret
 ):
     if not handle and not handle_file:
         raise TypeError("Must pass either a single --handle-file with mutiple handles")
@@ -45,9 +35,6 @@ def generate_time_map(
             times, times_tot_mins, sep_array = time_maps.tm_tools.generate(handle, all_tweets, SCATTER, save_path)
         if map_type is "heatmap" or map_type is "both":
             times, times_tot_mins, sep_array = time_maps.tm_tools.generate(handle, all_tweets, HEAT, save_path)
-
-
-
 
 
 if __name__ == "__main__":
